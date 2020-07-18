1d0b337a7411737323a6ac93fc3f995261f7707d
Ronit-Banerjea <ronitbanerjea@gmail.com>
2020-07-18 13:36:57 +0530

    18/07/2020; 13:36pm

--- file1.py
+++ file1.py
@@ -2,9 +2,17 @@
 """
 Topic: Test file
 """
-
 list1 = [1,2,3,4,5,"a",'Ronit', 'Banerjea',True]
 list1
 print("\nType of list:", type(list1))
-print("\nThis is: ", list1)
 
+z = int(input("type 1 for list, 2 to exit: "))
+if z==1:
+    print("\nList is: ", list1)
+elif z==2:
+    print("Now we leave!")
+else:
+    print("enter 1 or 2")
+
+
+    
