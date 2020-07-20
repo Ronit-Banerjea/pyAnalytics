84986d01bd273246c6be51bd8da08387d3cb5e40
Ronit-Banerjea <ronitbanerjea@gmail.com>
2020-07-20 21:42:32 +0530

    20/07/2020; 21:42

--- file1.py
+++ file1.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-"""
-Topic: Test file
-"""
+
+#Topic: Test file
+
 list1 = [1,2,3,4,5,"a",'Ronit', 'Banerjea',True]
 list1
 print("\nType of list:", type(list1))
@@ -13,6 +13,3 @@ elif z==2:
     print("Now we leave!")
 else:
     print("enter 1 or 2")
-
-
-    
